8fd1405473ca68beb18b5030e38301911901ec6c
vakasravani <63398913+vakasravani@users.noreply.github.com>
2020-04-12 02:28:11 -0700

    Update Case1_Commandlinearg.cpp

--- Technical Assessment 1/case 1/Case1_Commandlinearg.cpp
+++ Technical Assessment 1/case 1/Case1_Commandlinearg.cpp
@@ -12,38 +12,38 @@ int main(int argc,char *argv[])
 {
     if(argc>=2)
     {
-	    if(strcmp(argv[1],"-h")==0)							//if loop for comparing the input string with "-h"
+	    if(strcmp(argv[1],"-h")==0)				//if loop for comparing the input string with "-h"
 	    {
-            help();
+            	help();
 	    }
     else
     {
-	    int iI=1;
-	    cout<<argv[0]<<endl;								//printing the filename.exe by using argv[0]
-	    cout<<"type"<<"\t"<<"value"<<"\t"<<"size"<<endl;
-	    while(iI<argc)                                      //while loop for checking the argcount
-	    {
-		    int iA=atoi(argv[iI]);							//atoi function for converting string to int
-		    if(iA==0)										//if the 'if' loop is true then char and string will be displayed
-		    {
-			    if(strlen(argv[iI])==1)
-				    cout<<endl<<"char";
-			    else
-				    cout<<endl<<"string";
-				    cout<<"\t"<<argv[iI]<<"\t"<<strlen(argv[iI]);
-		    }
-		    else											//else case for displaying int and float values
-		    {
-			    float fB=atof(argv[iI]);
-			    if(iA==fB)
-			    {
-				    cout<<endl<<"int"<<"\t"<<iA<<"\t"<<sizeof(iA);
-			    }
-			    else
-				    cout<<endl<<"float"<<"\t"<<fB<<"\t"<<sizeof(fB);
+	int iI=1;
+	cout<<argv[0]<<endl;					//printing the filename.exe by using argv[0]
+	cout<<"type"<<"\t"<<"value"<<"\t"<<"size"<<endl;
+	while(iI<argc)                                      	//while loop for checking the argcount
+	{
+		int iA=atoi(argv[iI]);				//atoi function for converting string to int
+		if(iA==0)					//if the 'if' loop is true then char and string will be displayed
+		{
+			if(strlen(argv[iI])==1)
+				cout<<endl<<"char";
+			else
+				cout<<endl<<"string";
+				cout<<"\t"<<argv[iI]<<"\t"<<strlen(argv[iI]);
+		}
+		else						//else case for displaying int and float values
+		{
+			 float fB=atof(argv[iI]);
+			 if(iA==fB)
+			 {
+				 cout<<endl<<"int"<<"\t"<<iA<<"\t"<<sizeof(iA);
+			 }
+			 else
+				 cout<<endl<<"float"<<"\t"<<fB<<"\t"<<sizeof(fB);
 		}
 		iI++;
-		}
+	}
     }
     }
 }
