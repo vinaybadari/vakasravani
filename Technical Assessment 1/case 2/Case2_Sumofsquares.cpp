af70817b725f22f444de5a0a1f889cfc539a68e0
vakasravani <63398913+vakasravani@users.noreply.github.com>
2020-04-12 02:24:28 -0700

    Update Case2_Sumofsquares.cpp

--- Technical Assessment 1/case 2/Case2_Sumofsquares.cpp
+++ Technical Assessment 1/case 2/Case2_Sumofsquares.cpp
@@ -8,13 +8,13 @@
 using namespace std;
 void sumofsquares()
 {
-        int iI,iN,iSum=0;							//intialialized the required variables
+        int iI,iN,iSum=0;						//intialialized the required variables
         cout<<"calculating the sum of the series (1*1)+(2*2)+(3*3)+(4*4)+(5*5)+...+(n*n):"<<endl;
         cout<<"---------------------------------------------------------------------"<<endl;
         cout<<"enter the value for nth term:";		
         cin>>iN;								
         for(iI=1;iI<=iN;iI++) 						//for loop to calculate the sum of series up to the nth term
-	    {
+	{
             iSum+=iI*iI;							
         }
         cout<<"The sum of the above series is:"<<iSum<<endl;   
@@ -25,13 +25,13 @@ int main(int argc,char *argv[])
     {
 	    if(strcmp(argv[1],"-h")==0)					//if loop for comparing the input string with "-h"
 	    {
-            cout<<"Usage:The main purpose of this program is to calculate the sum of the squares of a given number which is the following format:\n(1*1)+(2*2)+(3*3)+......(n*n) "<<endl;
-            cout<<"input:4"<<endl;
-            cout<<"output:30"<<endl;
+            	cout<<"Usage:The main purpose of this program is to calculate the sum of the squares of a given number which is the following format:\n(1*1)+(2*2)+(3*3)+......(n*n) "<<endl;
+            	cout<<"input:4"<<endl;
+            	cout<<"output:30"<<endl;
 	    }
     }
-	else
-	{
+    else
+    {
         sumofsquares();
     }
     
