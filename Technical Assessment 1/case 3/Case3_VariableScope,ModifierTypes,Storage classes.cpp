372a236d6da480e731d48e722e4fa0b1ccac50c0
vakasravani <63398913+vakasravani@users.noreply.github.com>
2020-04-16 00:16:04 -0700

    Update Case3_VariableScope,ModifierTypes,Storage classes.cpp

--- Technical Assessment 1/case 3/Case3_VariableScope,ModifierTypes,Storage classes.cpp
+++ Technical Assessment 1/case 3/Case3_VariableScope,ModifierTypes,Storage classes.cpp
@@ -11,7 +11,7 @@ int iE;
 extern int iEx=20;
 void scope()                        	//variable scope function
 {
-	int iB=20;                      	//local variable
+	int iB=20;                      //local variable
 	cout<<"-----------------------------------------------------------"<<endl;
 	cout<<"                     VARIABLE SCOPE                        "<<endl;
 	cout<<"global variable a:"<<iA<<endl;   
@@ -35,18 +35,18 @@ void modifier()                     	//modifier types function
 void storage()                      	 //storage class function
 {
 	int iX=10;
-    float fY=3.5;
-    auto fZ=iX+fY;           		 //addition of two variable with auto storage class as return type
-    cout<<"                     STORAGE CLASSES                         "<<endl;
-    cout<<"value of auto variable z is                               : "<<fZ<<endl;
-    register int iR=10;             	//register storage classes initialization
-    cout<<"address of register variable r is                         : "<<&iR<<endl;
-    extern int iE;                 		//extern storage class declaration
-    cout<<"extern storage value before initialization is             : "<<iE<<endl;
-    iE=20;
-    cout<<"extern storage value after initialization is              : "<<iE<<endl;
-    int iAe=iEx+iE;
-    cout<<"value of ae after adding extern global & local variable is: "<<iAe<<endl;
+    	float fY=3.5;
+    	auto fZ=iX+fY;           	//addition of two variable with auto storage class as return type
+    	cout<<"                     STORAGE CLASSES                         "<<endl;
+    	cout<<"value of auto variable z is                               : "<<fZ<<endl;
+    	register int iR=10;             //register storage classes initialization
+    	cout<<"address of register variable r is                         : "<<&iR<<endl;
+    	extern int iE;                 	//extern storage class declaration
+    	cout<<"extern storage value before initialization is             : "<<iE<<endl;
+    	iE=20;
+    	cout<<"extern storage value after initialization is              : "<<iE<<endl;
+    	int iAe=iEx+iE;
+    	cout<<"value of ae after adding extern global & local variable is: "<<iAe<<endl;
     
 }
 void staticstorage()                	//static storage class function
@@ -59,12 +59,12 @@ class number                        	//mutable class
 {	
 	mutable int iNum;
 	public:
-		void getdata() const        	//getdata function which is kept as const
+		void getdata() const    //getdata function which is kept as const
 		{
 			iNum=10;
 			iNum++;
 		}
-		void display()              	//display function for displaying the mutable number
+		void display()          //display function for displaying the mutable number
 		{
 			getdata();
 			cout<<"mutable num                                               :"<<iNum<<endl;
@@ -75,22 +75,22 @@ int main(int argc,char *argv[])
 {
     if(argc>=2)
     {
-	    if(strcmp(argv[1],"-h")==0)	  	//if loop for comparing the input string with "-h"				
+	    if(strcmp(argv[1],"-h")==0)	 //if loop for comparing the input string with "-h"				
 	    {
-            cout<<"                                     USAGE                                          "<<endl;
-            cout<<"This program explains the usage of variable scope,modifier types & storage classes"<<endl;
+            	cout<<"                                     USAGE                                          "<<endl;
+            	cout<<"This program explains the usage of variable scope,modifier types & storage classes"<<endl;
 	    }
     }
     else
     {
         //calling functions
-	    scope();
-	    modifier();
-	    storage();
-	    staticstorage();
-	    cout<<"*** staticstorage variable after incrementation is ***"<<endl;
-	    staticstorage();
-	    number n;
-	    n.display();
+	scope();
+	modifier();
+	storage();
+	staticstorage();
+	cout<<"*** staticstorage variable after incrementation is ***"<<endl;
+	staticstorage();
+	number n;
+	n.display();
     }
 }
