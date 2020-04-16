cdd1abd58ac39d33be791e0dfb2bdca850729320
vakasravani <63398913+vakasravani@users.noreply.github.com>
2020-04-16 00:11:10 -0700

    Update Case1_helpfun.cpp

--- Technical Assessment 1/case 1/Case1_helpfun.cpp
+++ Technical Assessment 1/case 1/Case1_helpfun.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 void help()
 {
-    cout<<"Usage:The main purpose of this program is to calculate the datatype,value and sizeof the value"<<endl;
-    cout<<"user should give the input the following format:"<<endl;
-    cout<<"Input:"<<"\t"<<"1"<<"\t"<<"a"<<"\t"<<"sss"<<"\t"<<"4.5643"<<endl;
+    	cout<<"Usage:The main purpose of this program is to calculate the datatype,value and sizeof the value"<<endl;
+    	cout<<"user should give the input the following format:"<<endl;
+    	cout<<"Input:"<<"\t"<<"1"<<"\t"<<"a"<<"\t"<<"sss"<<"\t"<<"4.5643"<<endl;
 	cout<<"output is displayed in the form of:"<<endl;
 	cout<<"Type"<<"\t"<<"value"<<"\t"<<"sizeof"<<endl;
 	cout<<"char"<<"\t"<<"a"<<"\t"<<"1"<<endl;
