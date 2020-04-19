f4dfefe36b07f744384278ca5863f791d9d8ed1e
vakasravani <63398913+vakasravani@users.noreply.github.com>
2020-04-19 05:20:34 -0700

    Update Case2_Sumofsquares.cpp

--- Technical Assessment 1/case 2/Case2_Sumofsquares.cpp
+++ Technical Assessment 1/case 2/Case2_Sumofsquares.cpp
@@ -4,16 +4,18 @@
   filename	        : case2_Sumofsquares
   Author 	        : Naga sravani
   Creation Date   	: 07/04/2020
-  Modified Data     : 16/04/2020 */
+  Modified Data     	: 16/04/2020 */
   
 #include<iostream>
 #include<string.h>
 using namespace std;
 int sumofsquares(int iNum)
 {
-        int iI,iSum=0;							        //intialialized the required variables							
-        for(iI=1;iI<=iNum;iI++) 						//for loop to calculate the sum of series up to the nth term
-	    {
+	//intialialized the required variables
+        int iI,iSum=0;	
+	//for loop to calculate the sum of series up to the nth term
+        for(iI=1;iI<=iNum;iI++) 						
+	{
             iSum+=iI*iI;							
         }
         return iSum;  
@@ -22,21 +24,22 @@ int main(int argc,char *argv[])
 {
     if(argc==2)
     {
-	    if(strcmp(argv[1],"-h")==0)					//if loop for comparing the input string with "-h"
+	    //if loop for comparing the input string with "-h"
+	    if(strcmp(argv[1],"-h")==0)					
 	    {
-            cout<<"                                                         USAGE                                                           "<<endl;
-            cout<<"The main purpose of this program is to calculate the sum of the squares of a given number which is the following format:\n(1*1)+(2*2)+(3*3)+......(n*n) "<<endl;
-            cout<<"input : 4"<<endl;
-            cout<<"output: 30"<<endl;
+            	cout<<"                                                         USAGE                                                           "<<endl;
+            	cout<<"The main purpose of this program is to calculate the sum of the squares of a given number which is the following format:\n(1*1)+(2*2)+(3*3)+......(n*n) "<<endl;
+            	cout<<"input : 4"<<endl;
+            	cout<<"output: 30"<<endl;
 	    }
     }
-	else
-	{
-		int iNum,iTotal;
-		cout<<"calculating the sum of the series (1*1)+(2*2)+(3*3)+(4*4)+(5*5)+...+(n*n):"<<endl;
+    else
+    {
+	int iNum,iTotal;
+	cout<<"calculating the sum of the series (1*1)+(2*2)+(3*3)+(4*4)+(5*5)+...+(n*n):"<<endl;
         cout<<"---------------------------------------------------------------------"<<endl;
-		cout<<"Enter the input:";
-		cin>>iNum;
+	cout<<"Enter the input:";
+	cin>>iNum;
         iTotal=sumofsquares(iNum); 
         cout<<"The sum of the above series is:"<<iTotal<<endl; 
     }
